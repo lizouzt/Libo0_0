267e2575ac216c2503850cdc901f9b66e34e7333
Tao.z <Taoz@Taozs-MacBook-Pro.local>
2014-11-17 19:41:54 +0800

    add_tip_toggle

--- src/common/tip/tip.js
+++ src/common/tip/tip.js
@@ -1,6 +1,6 @@
 define(function(require) {
   var dom;
-  var html = '<div id="J_tip"><div class="content"></div> </div>';
+  var html = '<div id="J_tip"><div class="tip_content"></div> </div>';
   var tip = {
     show:function (msg) {
       var self = this;
@@ -8,11 +8,16 @@ define(function(require) {
         $(document.body).append(html);
         dom = $("#J_tip");
       }
-      dom.find(".content").text(msg);
+      dom.find(".tip_content").text(msg);
       dom.show();
     },
     hide:function () {
       dom.hide('display');
+    },
+    toggleDisplay: function(msg, time){
+      var self = this;
+      self.show(msg);
+      setTimeout(self.hide, time);
     }
   };
 
