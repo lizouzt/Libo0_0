fd11d61aa4b1989be97b0ce8e2432b6c01091743
Tao.z <lizouzt@gmail.com>
2015-11-04 19:54:55 +0800

    Update getAnchor.js

--- src/lib/widget/hash/getAnchor.js
+++ src/lib/widget/hash/getAnchor.js
@@ -1,6 +1,6 @@
 define(function (require) {
-    var url = /\?/.test(window.location.href) ? window.location.href.match(/\?(.+)/)[1] : "";
-    /\#\!/.test(url) && (url = url.replace(/\#\!.*/, ''));
+    var hash = /\#\!/.test(window.location.hash) ? window.location.hash.match(/\#\!(.+)/)[1] : "";
+        /\?/.test(hash) && (hash = hash.replace(/\?.*/, ''));
     return
         /**
          * 获取
@@ -8,7 +8,7 @@ define(function (require) {
          * @return {[type]}     [description]
          */
         function (key) {
-            var p = url.split('&');
+            var p = hash.split('&');
             var result = '';
             for(var i=0,len=p.length;i<len;i++){
                 var obj = p[i].split('=');
