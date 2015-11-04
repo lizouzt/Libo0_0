9ecdccf46cf4e78d2c2bd21e8c53463dd494a018
Tao.z <lizouzt@gmail.com>
2015-11-04 19:55:19 +0800

    Update parse.js

--- src/lib/widget/hash/parse.js
+++ src/lib/widget/hash/parse.js
@@ -1,11 +1,10 @@
 define(function(){
-    var hash = /\#\!/.test(window.location.hash) ? window.location.hash.match(/\#\!(.+)/)[0] : "";
-    /\?/.test(hash) && (hash = hash.replace(/\?.*/, ''));
+    var parse = /\?/.test(window.location.href) ? window.location.href.match(/\?(.+)/)[1] : "";
+    /\#\!/.test(parse) && (parse = parse.replace(/\#\!.*/, ''));
     return (function () {
         var obj = {};
-        if (hash.indexOf("#!" == 0)) {
-            hash = hash.substring(2);
-            var m = hash.split("&");
+        if (!!parse) {
+            var m = parse.split("&");
             for (var i in m) {
                 var t = m[i].split("=");
                 obj[t[0]] = decodeURI(t[1]);
